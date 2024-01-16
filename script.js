e1849d5788f4907dc49e313f3902bb510053974f
Kayri Monteiro <kayri.monteiro77@gmail.com>
2024-01-15 22:42:36 -0300

    inicio do javacript para o contador de tempo da msc

--- script.js
+++
@@ -1,8 +0,0 @@
-starmusic(1);
-stopmusic(2);
-
-function starmusic(number){
-
-if
-    console.log(number);
-}
---
+++ src/javascript/script.js
@@ -0,0 +1,10 @@
+var minutos = 15
+var segundos = 23
+
+    while (haveTime(segundos <= 59)){
+        takeTime(minutos - 1)
+    }
+
+
+
+console.log(timerMuscWorks)
